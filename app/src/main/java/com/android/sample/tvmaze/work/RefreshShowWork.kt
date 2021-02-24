49e7b4ab3642ec4aa92e902a47cd8c46fee67e02
Ali Rezaei <ali.rezaei09@gmail.com>
2021-02-24 15:18:13 +0330

    Using data for Result.success

--- app/src/main/java/com/android/sample/tvmaze/work/RefreshShowWork.kt
+++ app/src/main/java/com/android/sample/tvmaze/work/RefreshShowWork.kt
@@ -1,29 +1,35 @@
 package com.android.sample.tvmaze.work
 
+import android.annotation.SuppressLint
 import android.content.Context
 import androidx.work.CoroutineWorker
+import androidx.work.Data
 import androidx.work.WorkerParameters
 import com.android.sample.tvmaze.repository.ShowRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.koin.core.KoinComponent
 import org.koin.core.inject
-import timber.log.Timber
 
 class RefreshShowWork(appContext: Context, params: WorkerParameters) :
     CoroutineWorker(appContext, params), KoinComponent {
 
     companion object {
         const val WORK_NAME = "RefreshShowWork"
+        private const val WORK_DATA_KEY = "KeyShowWork"
     }
 
     /**
      * A coroutine-friendly method to do your work.
      */
+    @ExperimentalCoroutinesApi
     override suspend fun doWork(): Result {
         val repository: ShowRepository by inject()
         return try {
             val apiShows = repository.getShowsFromRemoteDataSource()
-            Timber.d("Size of items: %d", apiShows.size)
-            Result.success()
+
+            @SuppressLint("RestrictedApi")
+            val data = Data.Builder().put(WORK_DATA_KEY, apiShows).build()
+            Result.success(data)
         } catch (err: Exception) {
             Result.failure()
         }
