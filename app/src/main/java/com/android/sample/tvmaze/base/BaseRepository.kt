66a185bad76935631b682b974b8c848279ee6118
Ali Rezaei <ali.rezaei09@gmail.com>
2021-04-24 15:28:58 +0430

    More specific error message

--- app/src/main/java/com/android/sample/tvmaze/base/BaseRepository.kt
+++ app/src/main/java/com/android/sample/tvmaze/base/BaseRepository.kt
@@ -30,7 +30,8 @@ abstract class BaseRepository<T>(
                     refresh()
                     emit(Resource.success(query()))
                 } catch (err: Exception) {
-                    emit(Resource.error(context.getString(R.string.failed_loading_msg), it))
+                    emit(Resource.error(if (it.isEmpty()) context.getString(R.string.failed_loading_msg) else
+                        context.getString(R.string.failed_refresh_msg), it))
                 }
             } else {
                 emit(Resource.error(context.getString(R.string.failed_network_msg), it))
