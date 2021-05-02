0fb12bdb1141a1a0040d35e4795a806086aeb463
Ali Rezaei <ali.rezaei09@gmail.com>
2021-05-02 23:07:03 +0430

    Cleanup

--- app/src/main/java/com/android/sample/tvmaze/viewmodel/BaseListViewModel.kt
+++
@@ -1,6 +0,0 @@
-package com.android.sample.tvmaze.viewmodel
-
-import com.android.sample.tvmaze.base.BaseRepository
-
-open class BaseListViewModel<T>(repository: BaseRepository<List<T>>)
-    : BaseViewModel<List<T>>(repository) 
--- app/src/main/java/com/android/sample/tvmaze/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/android/sample/tvmaze/viewmodel/MainViewModel.kt
@@ -3,4 +3,4 @@ package com.android.sample.tvmaze.viewmodel
 import com.android.sample.tvmaze.domain.Show
 import com.android.sample.tvmaze.repository.ShowRepository
 
-class MainViewModel(repository: ShowRepository) : BaseListViewModel<Show>(repository)
+class MainViewModel(repository: ShowRepository) : BaseViewModel<List<Show>>(repository)
