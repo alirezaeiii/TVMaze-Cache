0205da401fc9e2dd9a7815c58de4e3784c63a56f
Ali Rezaei <ali.rezaei09@gmail.com>
2021-05-02 22:45:36 +0430

    Base class for ViewModel

---
+++ app/src/main/java/com/android/sample/tvmaze/viewmodel/BaseViewModel.kt
@@ -0,0 +1,31 @@
+package com.android.sample.tvmaze.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.android.sample.tvmaze.base.BaseRepository
+import com.android.sample.tvmaze.util.Resource
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+
+open class BaseViewModel<T>(
+        private val repository: BaseRepository<T>
+) : ViewModel() {
+
+    private val _shows = MutableStateFlow<Resource<T>>(Resource.loading())
+    val shows: StateFlow<Resource<T>>
+        get() = _shows
+
+    init {
+        refresh()
+    }
+
+    fun refresh() {
+        viewModelScope.launch {
+            repository.result.collect {
+                _shows.value = it
+            }
+        }
+    }
+}
--- app/src/main/java/com/android/sample/tvmaze/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/android/sample/tvmaze/viewmodel/MainViewModel.kt
@@ -1,34 +1,6 @@
 package com.android.sample.tvmaze.viewmodel
 
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.android.sample.tvmaze.domain.Show
 import com.android.sample.tvmaze.repository.ShowRepository
-import com.android.sample.tvmaze.util.Resource
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.launch
 
-@ExperimentalCoroutinesApi
-class MainViewModel(
-        private val repository: ShowRepository
-) : ViewModel() {
-
-    private val _shows = MutableStateFlow<Resource<List<Show>>>(Resource.loading())
-    val shows: StateFlow<Resource<List<Show>>>
-        get() = _shows
-
-    init {
-        refreshShows()
-    }
-
-    fun refreshShows() {
-        viewModelScope.launch {
-            repository.result.collect {
-                _shows.value = it
-            }
-        }
-    }
-}
+class MainViewModel(repository: ShowRepository) : BaseListViewModel<Show>(repository)
